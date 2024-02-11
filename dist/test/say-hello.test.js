17981f6af07e3dbe90d68c6a9e06c11701967b93
yulianto <jooliantoo@gmail.com>
2024-02-11 13:52:02 +0700

    kompilasi

---
+++ dist/test/say-hello.test.js
@@ -0,0 +1,7 @@
+import { sayHello } from "../src/say-hello";
+describe("sayHello", function () {
+    it("should say hello", function () {
+        const name = "yulianto";
+        expect(sayHello(name)).toBe(`Hello ${name}`);
+    });
+});
